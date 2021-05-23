1c34e2cae00072a62b221ba249a17ca0811623af
golfos ilias <iliasgolfos@gmail.com>
2021-05-23 12:04:46 +0300

    Updates

---
+++ precache-manifest.1a0f9c5430b0d68ad00d6ac5466553cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49809eaff9d68b02193fda773b65f7b",
+    "url": "/Auction-app/index.html"
+  },
+  {
+    "revision": "6798a27b455581503e8e",
+    "url": "/Auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "131d075caa51af29002b",
+    "url": "/Auction-app/static/js/2.215efd11.chunk.js"
+  },
+  {
+    "revision": "6798a27b455581503e8e",
+    "url": "/Auction-app/static/js/main.9d16199c.chunk.js"
+  },
+  {
+    "revision": "f7472452af8151a2b3cc",
+    "url": "/Auction-app/static/js/runtime-main.0409cdc2.js"
+  }
+]);
